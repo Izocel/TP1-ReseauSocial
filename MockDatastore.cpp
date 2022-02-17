48540269ed439f2b302f5c375a572bb24a098a29
Izocel <68454661+Izocel@users.noreply.github.com>
2022-02-17 06:15:17 -0500

    coquille de merge VS

--- MockDatastore.cpp
+++ MockDatastore.cpp
@@ -2,11 +2,9 @@
 
 json MockDatastore::getMembre(std::string membreUuid)
 {
-<<<<<<< HEAD
+
     return m_mapMembre.at(membreUuid);
-=======
-    return json();
->>>>>>> 8e7c447c98c4fd0ca08b6402b868569b235d6278
+
 }
 
 json MockDatastore::putMembre(json membreData)
