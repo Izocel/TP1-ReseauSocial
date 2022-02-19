1f9188e640b60cf6ea970b80091e0c4342a3fafa
Izocel <68454661+Izocel@users.noreply.github.com>
2022-02-19 00:09:54 -0500

    MockDatastore: ajout de gestion putMessage

--- MockDatastore.cpp
+++ MockDatastore.cpp
@@ -1,10 +1,24 @@
 #include "MockDatastore.h"
+#include <iostream>
 
 json MockDatastore::getMembre(std::string membreUuid)
 {
     try
     {
         return m_mapMembre.at(membreUuid);
+    }
+    catch (const std::exception&)
+    {
+        return json();
+    }
+    return json();
+}
+
+json MockDatastore::getMessages(std::string messageUuid)
+{
+    try
+    {
+        return m_mapMessages.at(messageUuid);
     }
     catch (const std::exception&)
     {
@@ -21,4 +35,14 @@ json MockDatastore::putMembre(json membreData)
     
     m_mapMembre[uuid] = membreData;
     return membreData;
+}
+
+json MockDatastore::putMessage(json messageData)
+{
+    std::string uuid = messageData.at("messageID");
+
+    if (uuid.empty()) return json();
+
+    m_mapMessages[uuid].push_back(messageData);
+    return messageData;
 }
--- MockDatastore.h
+++ MockDatastore.h
@@ -8,9 +8,12 @@ class MockDatastore
 {
 private:
 	std::map<std::string, json> m_mapMembre;
+	std::map<std::string, json> m_mapMessages;
 
 public:
 	json getMembre(std::string membreUuid);
+	json getMessages(std::string membreUuid);
 	json putMembre(json membreData);
+	json putMessage(json messageData);
 };
 
